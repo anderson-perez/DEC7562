29afe4f507348f5ea6a15c8bd5c55ce377d5c036
Prof. Anderson Luiz Fernandes Perez <38473840+anderson-perez@users.noreply.github.com>
2026-08-26 14:56:26 -0300

    Aula do dia 24/08.

--- Aula_24_08/main.c
+++ Aula_24_08/main.c
@@ -23,6 +23,10 @@ void config_ports();
 
 void criar_tarefa(unsigned int prior, f_ptr tarefa, t_state estado);
 
+void bubble_sort();
+int prior_scheduler();
+int rr_scheduler();
+
 void led_1();
 void led_2();
 void led_3();
@@ -37,9 +41,15 @@ int main()
     config_ports();
     config_timer1();
     
+    criar_tarefa(5, led_1, READY);
+    criar_tarefa(6, led_2, READY);
+    criar_tarefa(3, led_3, READY);
+    
     LIGAR_TIMER1();
     
     while (1) {
+        if (task_running < 0) continue;
+        
         tarefas[task_running].tarefa();
     }
     
@@ -49,7 +59,8 @@ int main()
 void __attribute__ ((interrupt, no_auto_psv)) _T1Interrupt(void)
 {
     IFS0bits.T1IF = 0;
-    task_running = (task_running+1) % 3;
+    //task_running = (task_running+1) % qtd_tasks;
+    task_running = prior_scheduler();
 }
 
 void config_timer1()
@@ -90,4 +101,34 @@ void criar_tarefa(unsigned int prior, f_ptr tarefa, t_state estado)
     tarefas[qtd_tasks].estado = estado;
     tarefas[qtd_tasks].prioridade = prior;
     tarefas[qtd_tasks++].tarefa = tarefa;
+}
+
+inline void bubble_sort()
+{
+    for (int i = 0; i < qtd_tasks; i++) {
+        for (int j = i + 1; j < qtd_tasks; j++) {
+            if (tarefas[j].prioridade > tarefas[i].prioridade) {
+                tcb_t t = tarefas[j];
+                tarefas[j] = tarefas[i];
+                tarefas[i] = t;
+            }
+        }
+    }
+}
+
+int prior_scheduler()
+{
+    return 0;
+    
+    bubble_sort();
+    for (int i = 0; i < qtd_tasks; i++) {
+        if (tarefas[i].estado == READY) return i;
+    }
+    
+    return -1;
+}
+
+int rr_scheduler()
+{
+    
 }
