6413f47f70cc0d7ad393ae30d0d17ce2b12c70ab
Prof. Anderson Luiz Fernandes Perez <38473840+anderson-perez@users.noreply.github.com>
2026-09-23 14:44:26 -0300

    Versão de 23/09.

--- AdidOS.X/user_app.c
+++ AdidOS.X/user_app.c
@@ -3,11 +3,14 @@
 #include <xc.h>
 #include "sync.h"
 
+sem_t semaforo;
+
 
 TASK tarefa_a(void)
 {
     while (1) {
         LATEbits.LATE0 ^= 1;
+        sem_wait(&semaforo);
     }
 }
 TASK tarefa_b(void)
@@ -15,6 +18,7 @@ TASK tarefa_b(void)
     while (1) {
         LATEbits.LATE1 ^= 1;
         task_delay(100);
+        sem_post(&semaforo);
     }
 }
 TASK tarefa_c(void)
@@ -29,6 +33,7 @@ void config_user_app(void)
     TRISEbits.TRISE0 = 0;
     TRISEbits.TRISE1 = 0;
     TRISEbits.TRISE2 = 0;
+    sem_init(&semaforo, 0);
 }
 
 
